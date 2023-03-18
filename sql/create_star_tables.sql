6111adea74b887388216fe53144089cde1d57fd7
AlexeyOm <alexey.omelchenko@gmail.com>
2023-03-18 23:28:10 +0300

    added file for star scheme tables creation

---
+++ sql/create_star_tables.sql
@@ -0,0 +1,95 @@
+-- измерение времени с гранулярностью минута
+create table sales.dim_time as
+select 
+	format_time('%H:%M', time(t_stamp)) as time_id,
+	cast(format_time('%H', time(t_stamp)) as int) as hour,
+	cast(format_time('%M', time(t_stamp)) as int) as minute,
+	(case when cast(format_time('%H', time(t_stamp)) as int) < 13 then
+		  1
+		else
+		  0
+		end) as is_before_lunch,
+	(case
+		when cast(format_time('%H', time(t_stamp)) as int) in (13,14) then
+		  1
+		else
+		  0
+		end) as is_lunch_time,
+	(case
+		when cast(format_time('%H', time(t_stamp)) as int) > 14 then
+		  1
+		else
+		  0
+		end) as is_after_lunch,
+from (
+	select * 
+	from unnest(
+		GENERATE_TIMESTAMP_ARRAY('2016-10-05 08:00:00', '2016-10-05 20:00:00', interval 1 minute))
+		as t_stamp
+    );
+
+-- измерение дат с гранулярностью день
+create table sales.dim_days as
+select
+  format_date('%f', d) as id,
+  d as full_date,
+  extract(year from d) as year,
+  extract(isoweek from d) as year_week,
+  extract(dayofyear from d) as year_day,
+  format_date('%q', d) as quarter,
+  extract(month from d) as month,
+  format_date('%b', d) as month_name,
+  mod(cast (format_date('%w', d) as int64)+6, 7)+1 as week_day,
+  format_date('%a', d) as day_name,
+  (case when format_date('%a', d) in ('sunday', 'saturday') then 0 else 1 end) as is_weekday,
+  (case when format_date('%a', d) in ('sunday', 'saturday') then 1 else 0 end) as is_weekend,
+  (case 
+	  when d in (select holiday from sales.holidays) then 
+	  	1 
+	  else 
+	  	0
+	  end
+	  ) as is_national_holiday,
+  (case
+	  when h.holiday is not null then
+		h.holiday_name
+	  else
+		'none'
+	  end) as national_holiday_name,
+from (
+  select
+    *
+  from
+    unnest(generate_date_array('2021-01-01', '2023-12-31', interval 1 day)) as d )
+  left join sales.holidays as h 
+  on d = h.holiday;
+
+create table netologyds.sales.dim_cities ( -- измерение городов  
+	city_id int64, -- идентификатор города  
+	city_name string -- название города  
+);  
+
+create table netologyds.sales.dim_branches ( -- измерение подразделений  
+	branch_id int64, -- идентификатор подразделения  
+	branch_name string -- название подразделения  
+);  
+
+create table netologyds.sales.dim_payment_types ( -- измерение типов платежных средств
+	payment_type_id int64, -- идентификатор платежного средства  
+	payment_type_name string -- название платежного средства  
+);  
+
+create table netologyds.sales.dim_genders ( -- измерение гендеров  
+	gender_id int64, -- идентификатор гендера  
+	gender_name string -- название гендера  
+);
+
+create table netologyds.sales.dim_member_statuses ( -- измерение статусов участника программы лояльности
+	member_status_id int64, -- идентификатор участника  
+	member_status_name string -- название статуса участника программы лояльности  
+);  
+
+create table netologyds.sales.dim_product_lines ( -- измерение групп товаров  
+	product_line_id int64, -- идентификатор группы товаров  
+	product_line_name string -- название группы товаров    
+);
