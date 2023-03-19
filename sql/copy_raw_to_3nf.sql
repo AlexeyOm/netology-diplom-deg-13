c5df8116d210cf7d7afcabae9488c86174ede750
AlexeyOm <alexey.omelchenko@gmail.com>
2023-03-19 15:05:03 +0300

    added procedure to copy raw data to 3rd normal form

---
+++ sql/copy_raw_to_3nf.sql
@@ -0,0 +1,19 @@
+create or replace procedure sales.raw_to_nf(filter_date string)
+begin
+
+  declare sql_string string;
+  set sql_string = (select concat('insert into sales.sales_nf (invoice_id, branch, city, product_line, payment_type, member_status, gender, transaction_time, amount, unit_price, cost, rating) ',
+  ' select r.Invoice_ID, b.branch_id, c.city_id, pl.product_line_id, pt.payment_type_id, ms.member_status_id, g.gender_id, datetime(concat(r.date, " ", r.time, ":00")), r.Quantity, r.Unit_price, r.cogs, r.Rating',
+  ' from `sales.raw-data` as r ',
+  ' left join sales.branches as b on b.branch_name = r.Branch ',
+  ' left join sales.cities as c on c.city_name = r.City ',
+  ' left join sales.member_statuses as ms on ms.member_status_name = r.Customer_type ',
+  ' left join sales.product_lines as pl on pl.product_line_name = r.Product_line ',
+  ' left join sales.payment_types as pt on pt.payment_type_name = r.Payment ',
+  ' left join sales.genders as g on g.gender_name = r.Gender ',
+  ' where r.date = "', filter_date, '"'
+  ));
+
+  execute immediate sql_string;
+
+end
