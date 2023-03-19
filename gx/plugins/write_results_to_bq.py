6e3b28812e6ae5138b52b111f185a1e78555949f
AlexeyOm <alexey.omelchenko@gmail.com>
2023-03-19 22:43:48 +0300

    redone the logging function, now reading json instead of validation result object

--- gx/plugins/write_results_to_bq.py
+++ gx/plugins/write_results_to_bq.py
@@ -16,8 +16,29 @@ class LogToDatabaseResultsAction(ValidationAction):
     def _run(self, validation_result_suite, **kwargs):
         # Складываем в unexpected_index_list номера всех невалидных строк
 
+        project="netologyds"
+        bucket_name = "sample-sales-23"
+        folder_path = "validation/sales_data_expectations"
+
+        # В folder_path ищем последний json файл, загружаем в json_data
+
+        client = storage.Client()
+        bucket = client.get_bucket(bucket_name)
+        prefix = folder_path if folder_path.endswith('/') else f"{folder_path}/"
+        blobs = bucket.list_blobs(prefix=prefix)
+        folders = set()
+        for blob in blobs:
+            folders.add(blob.name)
+        json_file = sorted(list(folders))[-1]
+
+
+        blob = bucket.get_blob(json_file)
+        json_data = json.loads(blob.download_as_text())
+
+
+
         unexpected_index_list = set()
-        for result in validation_result_suite['results']:
+        for result in json_data['results']:
             if 'unexpected_index_list' in result['result']:
                 unexpected_index_list.update(result['result']['unexpected_index_list'])
         invalid_rows = list(unexpected_index_list)
@@ -27,7 +48,7 @@ class LogToDatabaseResultsAction(ValidationAction):
         table_ref = client.dataset(dataset_name).table(table_name)
         table = client.get_table(table_ref)
 
-        rows_fetched = validation_result_suite['results'][0]['result']['observed_value'] # первый результат - проверка числа строк в файле
+        rows_fetched = json_data['results'][0]['result']['observed_value'] # первый результат - проверка числа строк в файле
         result_row = [{'loading_date': date.today().strftime("%Y-%m-%d"),
                        'rows_fetched': rows_fetched,
                        'invalid_rows': len(invalid_rows),
